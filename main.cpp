197dfac87e4e9064acbaa80ae6b9d29089042957
Nikita <nikuza.genesis@yandex.ru>
2023-02-24 01:03:31 +0300

    fix code

--- main.cpp
+++ main.cpp
@@ -6,8 +6,11 @@
 
 main()
 {
-    int a = 12;
-    int b = 44;
+    int a,b;
+    printf("a=");
+    scanf("%i", &a);
+    printf("b=");
+    scanf("%i", &b);
     if(a>b)
     {
         printf("Больше");
